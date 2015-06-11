c644726526004ebdb21e643995ca8f9b389e4369
ycg <youchg@lsec.cc.ac.cn>
2015-06-11 22:32:50 +0800

    Modify test program.

--- test_matrix.cpp
+++ test_matrix.cpp
@@ -10,11 +10,11 @@ int main()
 {   
     clock_t start, end;
     start = clock();
-    int multi = 4;
-    int nrow1 = 200*multi;
-    int ncol1 = 200*multi;
+    int multi = 1;
+    int nrow1 = 10*multi;
+    int ncol1 = 10*multi;
     int nrow2 = ncol1;
-    int ncol2 = 200*multi;
+    int ncol2 = 10*multi;
     MatrixDense<double> A(nrow1, ncol1, "aaa");
     A.SetAllValue(10, "dRANDOM");
     //A.WriteMatlabDense("./data/aaa.dat");
@@ -22,6 +22,30 @@ int main()
     MatrixDense<double> B(nrow2, ncol2, "bbb");
     B.SetAllValue(13, "dRANDOM");
     MatrixDense<double> C = A*B;
+    //MatrixDense<double> D = A.MultiplyDirect(B);
+    //MatrixDense<double> E = C-D;
+    //D.Show(9);
+    //E.Show(9);
+
+    
+    MatrixDense<int> A100x100_1;
+    A100x100_1.ReadMatlabDense("./data/imat100x100_1.dat");
+    A100x100_1.SetName("A100x100_1");
+    MatrixDense<int> A100x100_2;
+    A100x100_2.ReadMatlabDense("./data/imat100x100_2.dat");
+    A100x100_2.SetName("A100x100_2");
+    MatrixDense<int> M;
+    M = A100x100_1.MultiplySlice( 2, 10, 7, 15,
+	            A100x100_2, 22, 30, 35, 41 );
+    M.SetName("multi_slice");
+    //M.Show(9);
+    MatrixDense<int> N;
+    N = A100x100_1.MultiplySliceTransform(
+	                         2, 10,  7, 15,
+	            A100x100_2, 35, 41, 22, 30 );
+    N.SetName("multi_slice_transform");
+    //N.Show(9);
+	                             
     end = clock();
     double time = (double)(end - start) / CLOCKS_PER_SEC;
     printf("USING time = %f\n", time);
